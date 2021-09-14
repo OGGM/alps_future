55946552645050e78ef7e1c9c3fbf3657ce1bded
Zora Schirmeister <zora.schirmeister@student.uibk.ac.at>
2021-09-14 19:53:27 +0200

    add html with different languages

---
+++ text.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+supported_languages = ['en', 'de']
+
+trads = {
+    'Title': 
+        {
+            'en': 'European Alps: Future glacier evolution',
+            'de': 'Die Zukunft der Alpengletscher'
+        },  
+    
+    'instruction':
+        {
+            'en': 'This interactive graphic displays the projected relative volume change (in % from 2017) of all glaciers in the European Alps from 2017 to 2100 under three different [RCP scenarios](https://skepticalscience.com/rcp.php). <br> The scenario RCP2.6 is the most optimistic (low greenhouse gas emissions), while the scenario RCP8.5 is the most pessimistic (very high greenhouse gas emissions). <br> You can also compare the glacier evolution for the different alpine countries that still have glaciers. Here as well, the relative volume change refers to the total glacier volume in 2017 for the selected region.',
+         'de': 'Diese interaktive Graphik zeigt die vorraussichtlichen relativen Gletschervolumenänderungen (in % zu 2017) der Europäischen Alpen für drei verschiedene [RCP-Szenarien](https://skepticalscience.com/rcp.php) zwischen 2017 und 2100. Hierbei ist RCP2.6 das optimistischste Szenario (niedrige Treibhausgasemissionen), hingegen ist das Szenario RCP8.5 das pessimistischste (sehr hohe Treibhausgasemissionen). Auch die Entwicklung der Gletscher der einzelnen Alpenländer, vorausgesetzt sie haben noch Gletscher, können verglichen werden. Hierbei ist ebenfalls das Referenzvolumen das der Gletscher von 2017, jedoch für das jeweilige Land.'
+        }, 
+    
+    'sel_region':
+        {
+            'en': 'Select a region:',
+            'de': 'Wähle eine Region:'
+        },
+    
+    'sel_label':
+        {
+            'en': 'Region: ',
+            'de': 'Region: '
+        },
+    'alps_label':
+        {
+            'en': 'Entire Alps',
+            'de': 'Ganzer Alpenraum'
+        }
+        
+}
+
+
