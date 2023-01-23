41cda03b156c14a808fff90eadf66618be3d6e99
lilianschuster <lilianschuster1@gmail.com>
2023-01-23 10:45:53 +0100

    added new app with Rounce et al. data

--- text.py
+++ text.py
@@ -33,4 +33,36 @@ trads = {
         
 }
 
+trads_ssp = {
+    'Title': 
+        {
+            'en': 'European Alps: Future glacier evolution',
+            'de': 'Die Zukunft der Alpengletscher'
+        },  
+    
+    'instruction':
+        {
+            'en': 'This interactive graphic displays the projected relative volume change (in % from 2022) of all glaciers in the European Alps from 2022 to 2100 under four different [SSP scenarios](https://www.dkrz.de/en/communication/climate-simulations/cmip6-en/the-ssp-scenarios). <br> The scenario SSP1-2.6 is a rather optimistic scenario (low greenhouse gas emissions) with an average global warming of ΔT~+1.8°C until 2100 relative to 1850-1900. The scenario SSP5-8.5 is the most pessimistic scenario (very high greenhouse gas emissions, on average ΔT~+4.5°C). <br> You can also compare the glacier evolution for the different alpine countries that still have glaciers. Here as well, the volume change is shown relative to the total glacier volume in 2022 for the selected region.',
+         'de': 'Diese interaktive Graphik zeigt die vorraussichtlichen relativen Gletschervolumenänderungen (in % zu 2022) der Europäischen Alpen für vier verschiedene [SSP-Szenarien](https://www.dkrz.de/de/kommunikation/klimasimulationen/cmip6-de/die-ssp-szenarien?set_language=de) zwischen 2022 und 2100. Hierbei ist SSP1-2.6 ein relativ optimistisches Szenario (niedrige Treibhausgasemissionen) mit einer globalen mittleren Erwärmung von ΔT~+1.8°C bis 2100 (relativ zu 1850-1900 berechnet). Das Szenario SSP5-8.5 ist das pessimistischste (sehr hohe Treibhausgasemissionen, im Mittel ΔT~+4.5°C). Auch die Entwicklung der Gletscher der einzelnen Alpenländer, vorausgesetzt sie haben noch Gletscher, können verglichen werden. Hierbei ist ebenfalls das Referenzvolumen das der Gletscher von 2022, jedoch für das jeweilige Land.'
+        }, 
+    
+    'sel_region':
+        {
+            'en': 'Select a region:',
+            'de': 'Wähle eine Region:'
+        },
+    
+    'sel_label':
+        {
+            'en': 'Region: ',
+            'de': 'Region: '
+        },
+    'alps_label':
+        {
+            'en': 'Entire Alps',
+            'de': 'Ganzer Alpenraum'
+        }
+        
+}
+
 
