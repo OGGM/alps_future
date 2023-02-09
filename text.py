cf3316e6eafa28849d6ccdd8f55d35dd72eb2eb9
lilianschuster <lilianschuster1@gmail.com>
2023-02-09 16:44:37 +0100

    added notebook with Rounce et al data

--- text.py
+++ text.py
@@ -27,8 +27,8 @@ trads = {
         },
     'alps_label':
         {
-            'en': 'Entire Alps',
-            'de': 'Ganzer Alpenraum'
+            'en': 'Alps (entirely)',
+            'de': 'Alpen (gesamt)'
         }
         
 }
@@ -59,24 +59,26 @@ trads_ssp = {
         },
     'alps_label':
         {
-            'en': 'Entire Alps',
-            'de': 'Ganzer Alpenraum'
+            'en': 'Alps (entirely)',
+            'de': 'Alpen (gesamt)'
         }
+        
         
 }
 
 
-trads_ssp_rgi_region = {
+
+trads_temp_change= {
     'Title': 
         {
-            'en': 'Future glacier evolution',
-            'de': 'Die Zukunft aller Gletscher'
+            'en': 'European Alps: Future glacier evolution',
+            'de': 'Die Zukunft der Alpengletscher'
         },  
     
     'instruction':
         {
-            'en': 'This interactive graphic displays the projected relative volume change (in % from 2022) of all glaciers globally from 2022 to 2100 under four different [SSP scenarios](https://www.dkrz.de/en/communication/climate-simulations/cmip6-en/the-ssp-scenarios). <br> The scenario SSP1-2.6 is a rather optimistic scenario (low greenhouse gas emissions) with an average global warming of ΔT~+1.8°C until 2100 relative to 1850-1900. The scenario SSP5-8.5 is the most pessimistic scenario (very high greenhouse gas emissions, on average ΔT~+4.5°C). <br> You can also compare the glacier evolution for different regions in the world. Here as well, the volume change is shown relative to the total glacier volume in 2022 for the selected region. Note that we always show the [median](https://en.wikipedia.org/wiki/Median) and the lower and upper bound of the [95% confidence interval](https://www.bumc.bu.edu/public-health-learning-modules/lessons/module-12-introduction-and-learning-objectives/the-95-confidence-interval/) as a representation of uncertainties of climate models.',
-         'de': 'Diese interaktive Graphik zeigt die vorraussichtlichen relativen Gletschervolumenänderungen (in % zu 2022) der Europäischen Alpen für vier verschiedene [SSP-Szenarien](https://www.dkrz.de/de/kommunikation/klimasimulationen/cmip6-de/die-ssp-szenarien?set_language=de) zwischen 2022 und 2100. Hierbei ist SSP1-2.6 ein relativ optimistisches Szenario (niedrige Treibhausgasemissionen) mit einer globalen mittleren Erwärmung von ΔT~+1.8°C bis 2100 (relativ zu 1850-1900 berechnet). Das Szenario SSP5-8.5 ist das pessimistischste (sehr hohe Treibhausgasemissionen, im Mittel ΔT~+4.5°C). Auch die Entwicklung der Gletscher für verschiedene Regionen der Welt können verglichen werden. Hierbei ist ebenfalls das Referenzvolumen das der Gletscher von 2022, jedoch für die jeweilige Region. Wir zeigen jeweils den [Median](https://de.wikipedia.org/wiki/Median) und den unteren und oberen Teil des [95% Konfidenz-Intervalls](https://de.wikipedia.org/wiki/Konfidenzintervall), was die Unsicherheiten von Klimamodellen repräsentiert.'
+            'en': 'This interactive graphic displays the projected volume change in % relative to 2022 of all glaciers in the European Alps from 2022 to 2100 under four global warming scenarios ΔT. The scenarios correspond to the average warming until 2100 relative to 1850-1900. <br> With more greenhouse gas emissions, global warming increases further, and with that, more glacier mass melts. Discover the differences between the alpine countries & regions by selecting one and comparing it to the total Alpine glacier evolution! We always show the [median](https://en.wikipedia.org/wiki/Median), and if you hover with the mouse, the 5th and 95th percentile are shown. In the interval between, we expect that 90% of the values of different climate models lie.',
+         'de': 'Diese interaktive Graphik zeigt die vorraussichtlichen relativen Änderungen des Gletschervolumens (in % zu 2022) für alle Gletscher der Europäischen Alpen unter vier verschiedenen globalen Erwärmungsszenarien ΔT zwischen den Jahren 2022 und 2100 an. Die Szenarien entprechen der mittleren globalen Erwärmung bis 2100 im Vergleich zu 1850-1900. Mit mehr Treibhausgasemissionen, steigt die globale Erderwärmung weiter an, und dadurch schmelzen auch mehr Gletscher. Entdecke die Unterschiede zwischen den Alpenländern & Regionen im Vergleich zur Veränderung in den gesamten Alpen indem du eine spezielle Region auswählst. Wir zeigen jeweils den [Median](https://de.wikipedia.org/wiki/Median) und wenn man mit der Maus über die Linien fährt auch das 5% und 95%-Perzentil welches den Bereich zeigt wo wir 90% der Werte vermuten auf Grund der Unterschiede der Klimamodelle.'
         }, 
     
     'sel_region':
@@ -90,129 +92,30 @@ trads_ssp_rgi_region = {
             'en': 'Region: ',
             'de': 'Region: '
         },
-    'glob_label':
+    'alps_label':
         {
-            'en': 'Globally',
-            'de': 'Global'
+            'en': 'Alps (entirely)',
+            'de': 'Alpen (gesamt)'
         }
+        
         
 }
 
-trads_temp_change_rgi_region = {
-    'Title': 
-        {
-            'en': 'Future glacier evolution',
-            'de': 'Die Zukunft aller Gletscher'
-        },  
-    
-    'instruction':
-        {
-            'en': 'This interactive graphic displays the projected relative volume change (in % from 2022) of all glaciers globally from 2022 to 2100 under four global warming scenarios ΔT. The scenarios correspond to the average warming until 2100 relative to 1850-1900. <br> With more greenhouse gas emissions, global warming increases further and with that, more glacier mass melts. Discover the differences between the regions by selecting one and comparing it to the global evolution! We always show the [median](https://en.wikipedia.org/wiki/Median), and if you hover with the mouse, the lower and upper bound of the [90% confidence interval](https://www.bumc.bu.edu/public-health-learning-modules/lessons/module-12-introduction-and-learning-objectives/the-95-confidence-interval/) as a representation of uncertainties of climate models.',
-         'de': 'Diese interaktive Graphik zeigt die vorraussichtlichen relativen Gletschervolumenänderungen (in % zu 2022) für alle Gletscher der Welt unter vier verschiedenen globalen Erwärmungsszenarien ΔT zwischen den Jahren 2022 und 2100 an. Die Szenarien entprechen der mittleren globalen Erwärmung bis 2100 im Vergleich zu 1850-1900. Mit mehr Treibhausgasemissionen, steigt die globale Erderwärmung weiter an, und dadurch schmelzen auch mehr Gletscher. Entdecke die Unterschiede zwischen den Regionen im Vergleich zur globalen Veränderung indem du eine spezielle Region auswählst. Wir zeigen jeweils den [Median](https://de.wikipedia.org/wiki/Median) und wenn man mit der Maus drüber fährt auch den unteren und oberen Teil des [90% Konfidenz-Intervalls](https://de.wikipedia.org/wiki/Konfidenzintervall), was die Unsicherheiten von Klimamodellen repräsentiert.'
-        }, 
-    
-    'sel_region':
-        {
-            'en': 'Select a region:',
-            'de': 'Wähle eine Region:'
-        },
-    
-    'sel_label':
-        {
-            'en': 'Region: ',
-            'de': 'Region: '
-        },
-    'glob_label':
-        {
-            'en': 'Globally',
-            'de': 'Global'
-        }
-        
-}
+d_region_name_en = {'Alps':'Alps (entirely)',
+                    'Austria':'Austria',
+                    'Italy': 'Italy',
+                    'Switzerland': 'Switzerland',
+                    'France': 'France',
+                    'Germany':'Germany',
+                    'Oetztal/Pitztal':'Oetztal/Pitztal'}
 
+d_region_name_de = {'Alps':'Alpen (gesamt)',
+                    'Austria':'Österreich',
+                    'Italy': 'Italien',
+                    'Switzerland': 'Schweiz',
+                    'France': 'Frankreich',
+                    'Germany':'Deutschland',
+                    'Oetztal/Pitztal':'Oetztal/Pitztal'}
 
-d_reg_num_name_en={}
-d_reg_num_name_en['01'] = 'Alaska'
-d_reg_num_name_en['02'] = 'Western Canada & USA'
-d_reg_num_name_en['03'] = 'Arctic Canada North'
-d_reg_num_name_en['04'] = 'Arctic Canada South'
-d_reg_num_name_en['05'] = 'Greenland'
-d_reg_num_name_en['06'] = 'Iceland'
-d_reg_num_name_en['07'] = 'Svalbard and Jan Mayen'
-d_reg_num_name_en['08'] = 'Scandinavia'
-d_reg_num_name_en['09'] = 'Russian Arctic'
-d_reg_num_name_en['10'] = 'North Asia'
-d_reg_num_name_en['11'] = 'Central Europe'
-d_reg_num_name_en['12'] = 'Caucasus and Middle East'
-d_reg_num_name_en['13'] = 'Central Asia'
-d_reg_num_name_en['14'] = 'South Asia West'
-d_reg_num_name_en['15'] = 'South Asia East'
-d_reg_num_name_en['16'] = 'Low Latitudes'
-d_reg_num_name_en['17'] = 'Southern Andes'
-d_reg_num_name_en['18'] = 'New Zealand'
-d_reg_num_name_en['19'] = 'Antarctic and Subantarctic'
-    
-d_reg_num_name_de = {}
-d_reg_num_name_de['01'] = 'Alaska'
-d_reg_num_name_de['02'] = 'West-Kanada & USA'
-d_reg_num_name_de['03'] = 'nördliche kanadische Arktis'
-d_reg_num_name_de['04'] = 'südliche kanadische Arktis'
-d_reg_num_name_de['05'] = 'Grönland'
-d_reg_num_name_de['06'] = 'Island'
-d_reg_num_name_de['07'] = 'Inselgruppen Svalbard und Jan Mayen'
-d_reg_num_name_de['08'] = 'Skandinavien'
-d_reg_num_name_de['09'] = 'russischer Arktis'
-d_reg_num_name_de['10'] = 'North Asia'
-d_reg_num_name_de['11'] = 'Zentral-Europa'
-d_reg_num_name_de['12'] = 'Kaukasus und naher Osten'
-d_reg_num_name_de['13'] = 'Zentral-Asien'
-d_reg_num_name_de['14'] = 'Südwest-Asien'
-d_reg_num_name_de['15'] = 'Südost-Asien'
-d_reg_num_name_de['16'] = 'niedere Breiten'
-d_reg_num_name_de['17'] = 'südliche Anden'
-d_reg_num_name_de['18'] = 'Neuseeland'
-d_reg_num_name_de['19'] = 'Antarktis und Subantarktis'
-
-d_reg_num_name = {'en': d_reg_num_name_en,
-                 'de': d_reg_num_name_de}
-
-
-
-
-
-
-trads_temp_change_rgi_region_publication_version = {
-    'Title': 
-        {
-            'en': 'Future glacier evolution',
-            'de': 'Die Zukunft aller Gletscher'
-        },  
-    
-    'instruction':
-        {
-            'en': 'This interactive graphic displays the projected relative volume change (in % from 2015) of all glaciers globally from 2015 to 2100 under four global warming scenarios ΔT. The scenarios correspond to the average warming until 2100 relative to 1850-1900. <br> With more greenhouse gas emissions, global warming increases further and with that, more glacier mass melts. Discover the differences between the regions by selecting one and comparing it to the global evolution! We always show the [median](https://en.wikipedia.org/wiki/Median), and if you hover with the mouse, the lower and upper bound of the [95% confidence interval](https://www.bumc.bu.edu/public-health-learning-modules/lessons/module-12-introduction-and-learning-objectives/the-95-confidence-interval/) as a representation of uncertainties of climate models.',
-         'de': 'Diese interaktive Graphik zeigt die vorraussichtlichen relativen Gletschervolumenänderungen (in % zu 2015) für alle Gletscher der Welt unter vier verschiedenen globalen Erwärmungsszenarien ΔT zwischen den Jahren 2015 und 2100 an. Die Szenarien entprechen der mittleren globalen Erwärmung bis 2100 im Vergleich zu 1850-1900. Mit mehr Treibhausgasemissionen, steigt die globale Erderwärmung weiter an, und dadurch schmelzen auch mehr Gletscher. Entdecke die Unterschiede zwischen den Regionen im Vergleich zur globalen Veränderung indem du eine spezielle Region auswählst. Wir zeigen jeweils den [Median](https://de.wikipedia.org/wiki/Median) und wenn man mit der Maus drüber fährt auch den unteren und oberen Teil des [95% Konfidenz-Intervalls](https://de.wikipedia.org/wiki/Konfidenzintervall), was die Unsicherheiten von Klimamodellen repräsentiert.'
-        }, 
-    
-    'sel_region':
-        {
-            'en': 'Select a region:',
-            'de': 'Wähle eine Region:'
-        },
-    
-    'sel_label':
-        {
-            'en': 'Region: ',
-            'de': 'Region: '
-        },
-    'glob_label':
-        {
-            'en': 'Globally',
-            'de': 'Global'
-        }
-        
-}
-
-
-
-
+d_region_num = {'en': d_region_name_en,
+                 'de': d_region_name_de}
