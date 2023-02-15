11bf7d1b6f90f762ee73c8903a38dd4b73020fdb
Lilian Schuster <37300567+lilianschuster@users.noreply.github.com>
2023-02-15 17:28:26 +0100

    Added notebooks that show the Rounce et al. data (#9)
    * added new app with Rounce et al. data
    
    * removed typo
    
    * added new apps replacing ssp with deltaT
    
    * added notebook with Rounce et al data

--- text.py
+++ text.py
@@ -27,10 +27,95 @@ trads = {
         },
     'alps_label':
         {
-            'en': 'Entire Alps',
-            'de': 'Ganzer Alpenraum'
+            'en': 'Alps (entirely)',
+            'de': 'Alpen (gesamt)'
         }
         
 }
 
+trads_ssp = {
+    'Title': 
+        {
+            'en': 'European Alps: Future glacier evolution',
+            'de': 'Die Zukunft der Alpengletscher'
+        },  
+    
+    'instruction':
+        {
+            'en': 'This interactive graphic displays the projected relative volume change (in % from 2022) of all glaciers in the European Alps from 2022 to 2100 under four different [SSP scenarios](https://www.dkrz.de/en/communication/climate-simulations/cmip6-en/the-ssp-scenarios). <br> The scenario SSP1-2.6 is a rather optimistic scenario (low greenhouse gas emissions) with an average global warming of ΔT~+1.8°C until 2100 relative to 1850-1900. The scenario SSP5-8.5 is the most pessimistic scenario (very high greenhouse gas emissions, on average ΔT~+4.5°C). <br> You can also compare the glacier evolution for the different alpine countries that still have glaciers. Here as well, the volume change is shown relative to the total glacier volume in 2022 for the selected region. ',
+         'de': 'Diese interaktive Graphik zeigt die vorraussichtlichen relativen Gletschervolumenänderungen (in % zu 2022) der Europäischen Alpen für vier verschiedene [SSP-Szenarien](https://www.dkrz.de/de/kommunikation/klimasimulationen/cmip6-de/die-ssp-szenarien?set_language=de) zwischen 2022 und 2100. Hierbei ist SSP1-2.6 ein relativ optimistisches Szenario (niedrige Treibhausgasemissionen) mit einer globalen mittleren Erwärmung von ΔT~+1.8°C bis 2100 (relativ zu 1850-1900 berechnet). Das Szenario SSP5-8.5 ist das pessimistischste (sehr hohe Treibhausgasemissionen, im Mittel ΔT~+4.5°C). Auch die Entwicklung der Gletscher der einzelnen Alpenländer, vorausgesetzt sie haben noch Gletscher, können verglichen werden. Hierbei ist ebenfalls das Referenzvolumen das der Gletscher von 2022, jedoch für das jeweilige Land.'
+        }, 
+    
+    'sel_region':
+        {
+            'en': 'Select a region:',
+            'de': 'Wähle eine Region:'
+        },
+    
+    'sel_label':
+        {
+            'en': 'Region: ',
+            'de': 'Region: '
+        },
+    'alps_label':
+        {
+            'en': 'Alps (entirely)',
+            'de': 'Alpen (gesamt)'
+        }
+        
+        
+}
 
+
+
+trads_temp_change= {
+    'Title': 
+        {
+            'en': 'European Alps: Future glacier evolution',
+            'de': 'Die Zukunft der Alpengletscher'
+        },  
+    
+    'instruction':
+        {
+            'en': 'This interactive graphic displays the projected volume change in % relative to 2022 of all glaciers in the European Alps from 2022 to 2100 under four global warming scenarios ΔT. The scenarios correspond to the average warming until 2100 relative to 1850-1900. <br> With more greenhouse gas emissions, global warming increases further, and with that, more glacier mass melts. Discover the differences between the alpine countries & regions by selecting one and comparing it to the total Alpine glacier evolution! We always show the [median](https://en.wikipedia.org/wiki/Median), and if you hover with the mouse, the 5th and 95th percentile are shown. In the interval between, we expect that 90% of the values of different climate models lie.',
+         'de': 'Diese interaktive Graphik zeigt die vorraussichtlichen relativen Änderungen des Gletschervolumens (in % zu 2022) für alle Gletscher der Europäischen Alpen unter vier verschiedenen globalen Erwärmungsszenarien ΔT zwischen den Jahren 2022 und 2100 an. Die Szenarien entprechen der mittleren globalen Erwärmung bis 2100 im Vergleich zu 1850-1900. Mit mehr Treibhausgasemissionen, steigt die globale Erderwärmung weiter an, und dadurch schmelzen auch mehr Gletscher. Entdecke die Unterschiede zwischen den Alpenländern & Regionen im Vergleich zur Veränderung in den gesamten Alpen indem du eine spezielle Region auswählst. Wir zeigen jeweils den [Median](https://de.wikipedia.org/wiki/Median) und wenn man mit der Maus über die Linien fährt auch das 5% und 95%-Perzentil welches den Bereich zeigt wo wir 90% der Werte vermuten auf Grund der Unterschiede der Klimamodelle.'
+        }, 
+    
+    'sel_region':
+        {
+            'en': 'Select a region:',
+            'de': 'Wähle eine Region:'
+        },
+    
+    'sel_label':
+        {
+            'en': 'Region: ',
+            'de': 'Region: '
+        },
+    'alps_label':
+        {
+            'en': 'Alps (entirely)',
+            'de': 'Alpen (gesamt)'
+        }
+        
+        
+}
+
+d_region_name_en = {'Alps':'Alps (entirely)',
+                    'Austria':'Austria',
+                    'Italy': 'Italy',
+                    'Switzerland': 'Switzerland',
+                    'France': 'France',
+                    'Germany':'Germany',
+                    'Oetztal/Pitztal':'Oetztal/Pitztal'}
+
+d_region_name_de = {'Alps':'Alpen (gesamt)',
+                    'Austria':'Österreich',
+                    'Italy': 'Italien',
+                    'Switzerland': 'Schweiz',
+                    'France': 'Frankreich',
+                    'Germany':'Deutschland',
+                    'Oetztal/Pitztal':'Oetztal/Pitztal'}
+
+d_region_num = {'en': d_region_name_en,
+                 'de': d_region_name_de}
